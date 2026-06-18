83b06bddb8b91a22d5b0a2c4c373258ffa094f3a
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-06-18 19:19:15 -0300

    ajustes imports

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from models import Base
@@ -9,8 +8,7 @@ class Database:
     Classe interface entre banco e app
     """
     def __init__(self):
-        # # Base para nossos modelos
-        # Base = declarative_base()
+
 
         # Conectar ao banco
         engine = create_engine('sqlite:///exemplo.db')
