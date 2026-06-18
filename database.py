30be3c3671fde33df45b2b0f4cd4a04b1300fddf
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-06-18 19:38:53 -0300

    comentarios

--- database.py
+++ database.py
@@ -8,12 +8,10 @@ class Database:
     Classe interface entre banco e app
     """
     def __init__(self):
-
-
         # Conectar ao banco
         engine = create_engine('sqlite:///exemplo.db')
+        # Criar tabelas que não existem
         Base.metadata.create_all(engine)
-
         # Criar uma sessão
         Session = sessionmaker(bind=engine)
         self.session = Session()
