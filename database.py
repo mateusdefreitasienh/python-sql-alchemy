14646739234187fe963a989e9a5cbf1de6f8ffe1
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-06-18 19:12:00 -0300

    ajustes nos imports

--- database.py
+++ database.py
@@ -2,17 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from models import Base
+
 class Database:
     """
     Classe interface entre banco e app
     """
     def __init__(self):
-        # Base para nossos modelos
-        self.Base = declarative_base()
+        # # Base para nossos modelos
+        # Base = declarative_base()
 
         # Conectar ao banco
         engine = create_engine('sqlite:///exemplo.db')
-        self.Base.metadata.create_all(engine)
+        Base.metadata.create_all(engine)
 
         # Criar uma sessão
         Session = sessionmaker(bind=engine)
